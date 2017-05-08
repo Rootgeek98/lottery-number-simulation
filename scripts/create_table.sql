91917a34f12e99a44c5ce5b6d993f30c6dd6bb4c
Michael Chen <cwchen123@gmail.com>
2017-05-08 02:39:47 +0000

    Add SQL scripts

---
+++ scripts/create_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS lottery
+(id INT NOT NULL AUTO_INCREMENT,
+d date NOT NULL,
+b1 char(2) NOT NULL,
+b2 char(2) NOT NULL,
+b3 char(2) NOT NULL,
+b4 char(2) NOT NULL,
+b5 char(2) NOT NULL,
+b6 char(2) NOT NULL,
+b7 char(2) NOT NULL,
+PRIMARY KEY (id)
+);
